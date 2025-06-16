765e2e7d9eddfd3018cc838f1aa24a47670b62bb
uncodem <emurex185@gmail.com>
2025-06-17 02:55:06 +0800

    Added progloader struct

--- src/loader.zig
+++ src/loader.zig
@@ -1,5 +1,18 @@
 const std = @import("std");
 const vals = @import("vals.zig");
 
+const Value = vals.Value;
+
+pub const Program = struct {
+    allocator: std.mem.Allocator = undefined,
+    code: std.ArrayList(u8) = undefined,
+    constants: std.ArrayList(Value) = undefined,
+
+    pub fn deinit(self: Program) void {
+        self.code.deinit();
+        self.constants.deinit();
+    }
+};
+
 
 
