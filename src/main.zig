5adcf03c3cda1ea07e1680073699f094effb6125
uncodem <emurex185@gmail.com>
2025-06-20 16:36:38 +0800

    Fixed leaks

--- src/main.zig
+++ src/main.zig
@@ -8,14 +8,21 @@ const opc = opcodes.VmOpcode;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
     defer _ = gpa.deinit();
+    defer _ = gpa.detectLeaks();
+
     const allocator = gpa.allocator();
+    // const allocator = std.heap.c_allocator;
 
-    const code = [_]u8{ @intFromEnum(opc.OP_CONST), 0, @intFromEnum(opc.OP_DUMP), @intFromEnum(opc.OP_RET) };
+    //    const byte_data = [_]u8{0, 36, 0x00, 0x00, 0x00, 0, 33, 0x00, 0x00, 0x00, 1, 0x41, 0x42, 0x43, 0x44, 0x45, 0x00};
+    const byte_data = [_]u8{};
 
-    const const_data = [_]u8{ 1, 0x41, 0x42, 0x43, 0x00 };
+    const test_prog = [_]u8{ @intFromEnum(opc.OP_INPUT), @intFromEnum(opc.OP_CAST), 3, @intFromEnum(opc.OP_DUMP), @intFromEnum(opc.OP_RET) };
 
-    const prog = try loader.Program.init(allocator, &const_data, &code);
+    const prog = try loader.Program.init(allocator, &byte_data, &test_prog);
     defer prog.deinit();
 
-    std.debug.print("{any}", .{gpa.detectLeaks()});
+    var vm = try core.Vm.init(allocator, &prog);
+    defer vm.deinit();
+
+    try vm.run(std.io.getStdIn().reader());
 }
