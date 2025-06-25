377168fc0a0aa6b56ab0633e9a9364053c56c009
uncodem <emurex185@gmail.com>
2025-06-26 07:08:04 +0800

    Refactored parser with let* and added parenthesis support

--- src/parser.ml
+++ src/parser.ml
@@ -58,16 +58,22 @@ let rec string_of_ast = function
 let parser_init toks =
     {stream = Array.of_list toks; indx = 0}
 
-let parse_literal st =
-    match expect st is_number with
-        | Ok (tok, st') -> 
-            (match tok with
-                | Lexer.Number t -> Ok (Number t, st')
-                (* Unreachable *)
-                | _ -> Error UnexpectedToken)
+let ( let* ) r f =
+    match r with
+        | Ok x -> f x
         | Error e -> Error e
 
-let rec parse_factor st = 
+let rec parse_literal st =
+    let tok, st' = next st in
+    match tok with
+        | Some (Lexer.Number x) -> Ok (Number x, st')
+        | Some Lexer.LParen ->
+            let* (node, st2') = parse_term st' in
+            let* (_, st3') = expect st2' (function Lexer.RParen -> true | _ -> false) in
+            Ok (node, st3')
+    | _ -> Error UnexpectedToken
+
+and parse_factor st = 
     match parse_literal st with
         | Error e -> Error e
         | Ok (left, st') -> parse_factor_loop left st'
@@ -78,14 +84,12 @@ and parse_factor_loop left st =
             (match binop_of_char c with 
                 | Some (Mul | Div as op) -> 
                     let _, st' = next st in 
-                    (match parse_literal st' with
-                        | Error e -> Error e
-                        | Ok (right, st2') -> parse_factor_loop (Binary (op, left, right)) st2')
+                    let* (right, st2') = parse_literal st' in parse_factor_loop (Binary (op, left, right)) st2'
                 | _ -> Ok (left, st))
         | Some _ -> Ok (left, st)
         | None -> Ok (left, st)
 
-let rec parse_term st = 
+and parse_term st = 
     match parse_factor st with
         | Error e -> Error e
         | Ok (left, st') -> parse_term_loop left st'
@@ -96,9 +100,7 @@ and parse_term_loop left st =
             (match binop_of_char c with 
                 | Some (Add | Sub as op) -> 
                     let _, st' = next st in 
-                    (match parse_factor st' with
-                        | Error e -> Error e
-                        | Ok (right, st2') -> parse_term_loop (Binary (op, left, right)) st2')
+                    let* (right, st2') = parse_factor st' in parse_term_loop (Binary (op, left, right)) st2'
                 | _ -> Ok (left, st))
         | Some _ -> Ok (left, st)
         | None -> Ok (left, st)
