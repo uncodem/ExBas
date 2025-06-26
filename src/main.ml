e95d3d1350ce5b2409da74f258f980305920027b
uncodem <emurex185@gmail.com>
2025-06-27 00:15:44 +0800

    Tweaked .ocamlformat to use 4-wide indents

--- src/main.ml
+++ src/main.ml
@@ -1,8 +1,8 @@
 let () =
-  let res =
-    Lexer.lexer_init "random_functoin (12, 12, 32, 41)"
-    |> Parser.parser_init |> Parser.parse_all
-  in
-  match res with
-  | Ok a -> print_endline (Parser.string_of_ast a)
-  | Error e -> Parser.parser_report e
+    let res =
+        Lexer.lexer_init "random_functoin (12, 12, 32, 41)"
+        |> Parser.parser_init |> Parser.parse_all
+    in
+    match res with
+    | Ok a -> print_endline (Parser.string_of_ast a)
+    | Error e -> Parser.parser_report e
