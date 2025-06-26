c3f7dbbb2dd1239020a09d44827794379abbe878
uncodem <emurex185@gmail.com>
2025-06-26 23:57:11 +0800

    Ran dune fmt

--- src/lexer.ml
+++ src/lexer.ml
@@ -1,105 +1,106 @@
-
 type token_pos = int
 
 type token =
-    | Ident of string * token_pos 
-    | Number of int * token_pos
-    | Oper of char * token_pos
-    | LParen of token_pos
-    | RParen of token_pos
-    | Comma of token_pos
-    | Illegal of string * token_pos
+  | Ident of string * token_pos
+  | Number of int * token_pos
+  | Oper of char * token_pos
+  | LParen of token_pos
+  | RParen of token_pos
+  | Comma of token_pos
+  | Illegal of string * token_pos
 
 type lexerstate = {
-    code: string;
-    position: int;
-    tokens: token list;
-    line_number: int;
+  code : string;
+  position : int;
+  tokens : token list;
+  line_number : int;
 }
 
-let lexer_advance {code; position; tokens; line_number} = 
-    {code = code; position = position + 1; tokens = tokens; line_number = line_number}
+let lexer_advance { code; position; tokens; line_number } =
+  { code; position = position + 1; tokens; line_number }
 
-let add_token {code; position; tokens; line_number} token =
-    { code = code; position = position; tokens = token :: tokens ; line_number = line_number}
+let add_token { code; position; tokens; line_number } token =
+  { code; position; tokens = token :: tokens; line_number }
 
-let peek {code; position; _} =
-    if position < String.length code then
-        Some code.[position]
-    else
-        None
+let peek { code; position; _ } =
+  if position < String.length code then Some code.[position] else None
 
 let is_alpha = function '_' | 'a' .. 'z' | 'A' .. 'Z' -> true | _ -> false
 let is_digit = function '0' .. '9' -> true | _ -> false
 let is_alphanum = function '0' .. '9' -> true | c -> is_alpha c
 let is_oper = function '+' | '-' | '*' | '/' | '%' -> true | _ -> false
-
 let is_special_char = function '(' | ')' | ',' -> true | _ -> false
 
-let token_of_special_char state = function 
-    | '(' -> Some (LParen state.line_number)
-    | ')' -> Some (RParen state.line_number)
-    | ',' -> Some (Comma state.line_number)
-    | _ -> None
-
+let token_of_special_char state = function
+  | '(' -> Some (LParen state.line_number)
+  | ')' -> Some (RParen state.line_number)
+  | ',' -> Some (Comma state.line_number)
+  | _ -> None
 
 let print_token = function
-    | Ident (s, _) -> print_endline ("Ident(" ^ s ^ ")")
-    | Number (x, _) -> print_endline ("Number(" ^ (string_of_int x) ^ ")")
-    | Oper (x, _) -> print_endline ("Oper(" ^ (String.make 1 x) ^ ")")
-    | Illegal (s, _) -> print_endline ("Illegal(" ^ s ^ ")")
-    | LParen _ -> print_endline "LParen"
-    | RParen _ -> print_endline "RParen"
-    | Comma _ -> print_endline "Comma"
+  | Ident (s, _) -> print_endline ("Ident(" ^ s ^ ")")
+  | Number (x, _) -> print_endline ("Number(" ^ string_of_int x ^ ")")
+  | Oper (x, _) -> print_endline ("Oper(" ^ String.make 1 x ^ ")")
+  | Illegal (s, _) -> print_endline ("Illegal(" ^ s ^ ")")
+  | LParen _ -> print_endline "LParen"
+  | RParen _ -> print_endline "RParen"
+  | Comma _ -> print_endline "Comma"
 
 let errorstring_of_token = function
-    | Ident (s, line) -> "Identifier \"" ^ s ^ "\" of line " ^ (string_of_int line)
-    | Number (x, line) -> "Number " ^ (string_of_int x) ^ " of line " ^ (string_of_int line)
-    | Oper (x, line) -> "Operator " ^ (String.make 1 x) ^ " of line " ^ (string_of_int line)
-    | Illegal (s, line) -> "Illegal token \"" ^ s ^ "\" of line " ^ (string_of_int line)
-    | LParen line -> "LParen ( of line " ^ (string_of_int line)
-    | RParen line -> "RParen ) of line " ^ (string_of_int line)
-    | Comma line -> "Comma , of line " ^ (string_of_int line)
+  | Ident (s, line) -> "Identifier \"" ^ s ^ "\" of line " ^ string_of_int line
+  | Number (x, line) ->
+      "Number " ^ string_of_int x ^ " of line " ^ string_of_int line
+  | Oper (x, line) ->
+      "Operator " ^ String.make 1 x ^ " of line " ^ string_of_int line
+  | Illegal (s, line) ->
+      "Illegal token \"" ^ s ^ "\" of line " ^ string_of_int line
+  | LParen line -> "LParen ( of line " ^ string_of_int line
+  | RParen line -> "RParen ) of line " ^ string_of_int line
+  | Comma line -> "Comma , of line " ^ string_of_int line
 
-let add_token_advance state t = 
-    add_token state t |> lexer_advance
+let add_token_advance state t = add_token state t |> lexer_advance
 
-let rec lex_none ({position; line_number; _} as state) =
-    match peek state with
-        | Some '\n' ->
-            {state with line_number = line_number + 1}
-            |> lexer_advance
-            |> lex_none
-        | Some c when Char.code c <= 32 -> lex_none (lexer_advance state)
-        | Some c when is_oper c -> lex_none (add_token_advance state (Oper (c, line_number)))
-        | Some c when is_digit c -> lex_number state 0
-        | Some c when is_alpha c -> lex_ident state position 0
-        | Some c when is_special_char c -> 
-            (match token_of_special_char state c with
-                | Some t -> lex_none (add_token_advance state t)
-                | None -> assert false)
-        | Some _ -> lex_illegal state position 0 line_number 
-        | None -> state.tokens
+let rec lex_none ({ position; line_number; _ } as state) =
+  match peek state with
+  | Some '\n' ->
+      { state with line_number = line_number + 1 } |> lexer_advance |> lex_none
+  | Some c when Char.code c <= 32 -> lex_none (lexer_advance state)
+  | Some c when is_oper c ->
+      lex_none (add_token_advance state (Oper (c, line_number)))
+  | Some c when is_digit c -> lex_number state 0
+  | Some c when is_alpha c -> lex_ident state position 0
+  | Some c when is_special_char c -> (
+      match token_of_special_char state c with
+      | Some t -> lex_none (add_token_advance state t)
+      | None -> assert false)
+  | Some _ -> lex_illegal state position 0 line_number
+  | None -> state.tokens
 
 and lex_illegal state start len line_num =
-    match peek state with
-        | Some c when Char.code c <= 32 -> lex_none (add_token state (Illegal ((String.sub state.code start len), line_num)))
-        | None -> lex_none (add_token state (Illegal ((String.sub state.code start len), line_num)))
-        | _ -> lex_illegal (lexer_advance state) start (len+1) line_num
+  match peek state with
+  | Some c when Char.code c <= 32 ->
+      lex_none
+        (add_token state (Illegal (String.sub state.code start len, line_num)))
+  | None ->
+      lex_none
+        (add_token state (Illegal (String.sub state.code start len, line_num)))
+  | _ -> lex_illegal (lexer_advance state) start (len + 1) line_num
 
 and lex_ident state start len =
-    match peek state with
-        | Some c when is_alphanum c -> lex_ident (lexer_advance state) start (len+1)
-        | _ -> lex_none (add_token state (Ident ((String.sub state.code start len), state.line_number)))
+  match peek state with
+  | Some c when is_alphanum c -> lex_ident (lexer_advance state) start (len + 1)
+  | _ ->
+      lex_none
+        (add_token state
+           (Ident (String.sub state.code start len, state.line_number)))
 
 and lex_number state acc =
-    match peek state with
-        | Some c when is_digit c -> 
-            let digit = Char.code c - Char.code '0' in
-            let new_acc = acc * 10 + digit in
-            lex_number (lexer_advance state) new_acc
-        | _ ->
-            lex_none (add_token state (Number (acc, state.line_number)))
+  match peek state with
+  | Some c when is_digit c ->
+      let digit = Char.code c - Char.code '0' in
+      let new_acc = (acc * 10) + digit in
+      lex_number (lexer_advance state) new_acc
+  | _ -> lex_none (add_token state (Number (acc, state.line_number)))
 
-let lexer_init code = 
-    lex_none { code = code; position = 0; tokens = []; line_number = 1} |> List.rev
+let lexer_init code =
+  lex_none { code; position = 0; tokens = []; line_number = 1 } |> List.rev
--- src/parser.ml
+++ src/parser.ml
@@ -1,164 +1,163 @@
-type parserstate = {
-    stream: Lexer.token array;
-    indx: int;
-}
+type parserstate = { stream : Lexer.token array; indx : int }
 
-let peek {indx; stream} = 
-    if indx >= Array.length stream then None
-    else Some stream.(indx)
+let peek { indx; stream } =
+  if indx >= Array.length stream then None else Some stream.(indx)
 
 let next st =
-    match peek st with
-        | Some t -> Some t, {st with indx = st.indx + 1}
-        | None -> None, st
+  match peek st with
+  | Some t -> (Some t, { st with indx = st.indx + 1 })
+  | None -> (None, st)
 
-type parser_error =
-    | UnexpectedToken of Lexer.token * string
-    | UnexpectedEOF
+type parser_error = UnexpectedToken of Lexer.token * string | UnexpectedEOF
 
-let expect st pred fail_string = 
-    let (tok, st') = next st in
-        match tok with 
-            | Some tok when pred tok -> Ok (tok, st')
-            | Some x -> Error (UnexpectedToken (x, fail_string))
-            | None -> Error UnexpectedEOF
+let expect st pred fail_string =
+  let tok, st' = next st in
+  match tok with
+  | Some tok when pred tok -> Ok (tok, st')
+  | Some x -> Error (UnexpectedToken (x, fail_string))
+  | None -> Error UnexpectedEOF
 
-let expect_lparen st = expect st (function Lexer.LParen _ -> true | _ -> false) "Expected LParen."
-let expect_rparen st = expect st (function Lexer.RParen _ -> true | _ -> false) "Expected RParen."
-let expect_ident st = expect st (function Lexer.Ident _ -> true | _ -> false) "Expected Identifier."
+let expect_lparen st =
+  expect st (function Lexer.LParen _ -> true | _ -> false) "Expected LParen."
 
-type binop = 
-    | Add
-    | Sub
-    | Mul
-    | Div
+let expect_rparen st =
+  expect st (function Lexer.RParen _ -> true | _ -> false) "Expected RParen."
+
+let expect_ident st =
+  expect st
+    (function Lexer.Ident _ -> true | _ -> false)
+    "Expected Identifier."
+
+type binop = Add | Sub | Mul | Div
 
 type ast_node =
-    | Number of int
-    | Unary of binop * ast_node
-    | Binary of binop * ast_node * ast_node
-    | Statement of string * ast_node list
+  | Number of int
+  | Unary of binop * ast_node
+  | Binary of binop * ast_node * ast_node
+  | Statement of string * ast_node list
 
-let is_number = function
-    | Lexer.Number _ -> true
-    | _ -> false
+let is_number = function Lexer.Number _ -> true | _ -> false
 
 let string_of_binop = function
-    | Add -> "+"
-    | Sub -> "-"
-    | Mul -> "*"
-    | Div -> "/"
+  | Add -> "+"
+  | Sub -> "-"
+  | Mul -> "*"
+  | Div -> "/"
 
 let binop_of_char = function
-    | '+' -> Some Add
-    | '-' -> Some Sub
-    | '*' -> Some Mul
-    | '/' -> Some Div
-    | _ -> None
-
+  | '+' -> Some Add
+  | '-' -> Some Sub
+  | '*' -> Some Mul
+  | '/' -> Some Div
+  | _ -> None
 
 let rec string_of_ast = function
-    | Number x -> string_of_int x
-    | Binary (a, x, y) ->  "(" ^ (string_of_binop a) ^ " " ^ (string_of_ast x) ^ " " ^ (string_of_ast y) ^ ")"
-    | Unary (a, x) -> "(" ^ (string_of_binop a) ^ " " ^ (string_of_ast x) ^ ")"
-    | Statement (call, params) -> 
-        let param_strs = List.map string_of_ast params in
-        let body = String.concat " " (call :: param_strs) in 
-        "(" ^ body ^ ")"
+  | Number x -> string_of_int x
+  | Binary (a, x, y) ->
+      "(" ^ string_of_binop a ^ " " ^ string_of_ast x ^ " " ^ string_of_ast y
+      ^ ")"
+  | Unary (a, x) -> "(" ^ string_of_binop a ^ " " ^ string_of_ast x ^ ")"
+  | Statement (call, params) ->
+      let param_strs = List.map string_of_ast params in
+      let body = String.concat " " (call :: param_strs) in
+      "(" ^ body ^ ")"
 
-let parser_init toks =
-    {stream = Array.of_list toks; indx = 0}
-
-let ( let* ) r f =
-    match r with
-        | Ok x -> f x
-        | Error e -> Error e
+let parser_init toks = { stream = Array.of_list toks; indx = 0 }
+let ( let* ) r f = match r with Ok x -> f x | Error e -> Error e
 
 let rec parse_literal st =
-    let tok, st' = next st in
-    match tok with
-        | Some (Lexer.Number (x, _)) -> Ok (Number x, st')
-        | Some (Lexer.LParen _) ->
-            let* (node, st2') = parse_term st' in
-            let* (_, st3') = expect_rparen st2' in
-            Ok (node, st3')
-        | Some x -> Error (UnexpectedToken (x, "Expected a literal (number or parenthesized expression)."))
-        | None -> Error UnexpectedEOF
+  let tok, st' = next st in
+  match tok with
+  | Some (Lexer.Number (x, _)) -> Ok (Number x, st')
+  | Some (Lexer.LParen _) ->
+      let* node, st2' = parse_term st' in
+      let* _, st3' = expect_rparen st2' in
+      Ok (node, st3')
+  | Some x ->
+      Error
+        (UnexpectedToken
+           (x, "Expected a literal (number or parenthesized expression)."))
+  | None -> Error UnexpectedEOF
 
 and parse_unary st =
-    match peek st with
-        | Some (Lexer.Oper ('-', _)) ->
-            let _, st' = next st in 
-            let* (node, st2') = parse_unary st' in
-            Ok (Unary (Sub, node), st2')
-        | _ -> parse_literal st
+  match peek st with
+  | Some (Lexer.Oper ('-', _)) ->
+      let _, st' = next st in
+      let* node, st2' = parse_unary st' in
+      Ok (Unary (Sub, node), st2')
+  | _ -> parse_literal st
 
-and parse_factor st = 
-    let* (left, st') = parse_unary st in
-    parse_factor_loop left st'
+and parse_factor st =
+  let* left, st' = parse_unary st in
+  parse_factor_loop left st'
 
-and parse_factor_loop left st = 
-    match peek st with
-        | Some (Lexer.Oper (c, _)) -> 
-            (match binop_of_char c with 
-                | Some (Mul | Div as op) -> 
-                    let _, st' = next st in 
-                    let* (right, st2') = parse_unary st' in parse_factor_loop (Binary (op, left, right)) st2'
-                | _ -> Ok (left, st))
-        | Some _ -> Ok (left, st)
-        | None -> Ok (left, st)
+and parse_factor_loop left st =
+  match peek st with
+  | Some (Lexer.Oper (c, _)) -> (
+      match binop_of_char c with
+      | Some ((Mul | Div) as op) ->
+          let _, st' = next st in
+          let* right, st2' = parse_unary st' in
+          parse_factor_loop (Binary (op, left, right)) st2'
+      | _ -> Ok (left, st))
+  | Some _ -> Ok (left, st)
+  | None -> Ok (left, st)
 
-and parse_term st = 
-    let* (left, st') = parse_factor st in
-    parse_term_loop left st'
+and parse_term st =
+  let* left, st' = parse_factor st in
+  parse_term_loop left st'
 
-and parse_term_loop left st = 
-    match peek st with
-        | Some (Lexer.Oper (c, _)) -> 
-            (match binop_of_char c with 
-                | Some (Add | Sub as op) -> 
-                    let _, st' = next st in 
-                    let* (right, st2') = parse_factor st' in parse_term_loop (Binary (op, left, right)) st2'
-                | _ -> Ok (left, st))
-        | Some _ -> Ok (left, st)
-        | None -> Ok (left, st)
+and parse_term_loop left st =
+  match peek st with
+  | Some (Lexer.Oper (c, _)) -> (
+      match binop_of_char c with
+      | Some ((Add | Sub) as op) ->
+          let _, st' = next st in
+          let* right, st2' = parse_factor st' in
+          parse_term_loop (Binary (op, left, right)) st2'
+      | _ -> Ok (left, st))
+  | Some _ -> Ok (left, st)
+  | None -> Ok (left, st)
 
 let parse_expr = parse_term
 
 let rec parse_param_list_loop st acc =
-    let* (expr, st') = parse_expr st in
-    match peek st' with
-        | Some (Lexer.Comma _) -> let _, st2' = next st' in parse_param_list_loop st2' (expr :: acc)
-        | _ -> Ok ((expr :: acc) |> List.rev, st')
+  let* expr, st' = parse_expr st in
+  match peek st' with
+  | Some (Lexer.Comma _) ->
+      let _, st2' = next st' in
+      parse_param_list_loop st2' (expr :: acc)
+  | _ -> Ok (expr :: acc |> List.rev, st')
 
-let parse_param_list st = 
-    let* (_, st') = expect_lparen st in
-    match peek st' with
-        | Some (Lexer.RParen _) -> 
-            let (_, st2') = next st' in Ok ([], st2')
-        | _ -> 
-            let* (params, st2') = parse_param_list_loop st' [] in
-            let* (_, st3') = expect_rparen st2' in
-            Ok (params, st3')
+let parse_param_list st =
+  let* _, st' = expect_lparen st in
+  match peek st' with
+  | Some (Lexer.RParen _) ->
+      let _, st2' = next st' in
+      Ok ([], st2')
+  | _ ->
+      let* params, st2' = parse_param_list_loop st' [] in
+      let* _, st3' = expect_rparen st2' in
+      Ok (params, st3')
 
-let parse_stmt st = 
-    let* (tok, st') = expect_ident st in
-    match tok with
-        | Lexer.Ident (token_name, _) -> 
-            let* (param_list, st2') = parse_param_list st' in
-            Ok ((Statement (token_name, param_list)), st2')
-        | _ -> assert false (* Unreachable *)
-
+let parse_stmt st =
+  let* tok, st' = expect_ident st in
+  match tok with
+  | Lexer.Ident (token_name, _) ->
+      let* param_list, st2' = parse_param_list st' in
+      Ok (Statement (token_name, param_list), st2')
+  | _ -> assert false (* Unreachable *)
 
 let parse_all st =
-    let* (node, st') = parse_stmt st in
-    match peek st' with
-        | Some x -> Error (UnexpectedToken (x, "Expected end of input."))
-        | None -> Ok node
+  let* node, st' = parse_stmt st in
+  match peek st' with
+  | Some x -> Error (UnexpectedToken (x, "Expected end of input."))
+  | None -> Ok node
 
 let parser_report = function
-    | UnexpectedEOF -> print_endline "parser: UnexpectedEOF"
-    | UnexpectedToken (x, fail_string) -> 
-        print_endline ("parser: UnexpectedToken " ^ (Lexer.errorstring_of_token x) ^ ". " ^ fail_string)
-
-
+  | UnexpectedEOF -> print_endline "parser: UnexpectedEOF"
+  | UnexpectedToken (x, fail_string) ->
+      print_endline
+        ("parser: UnexpectedToken "
+        ^ Lexer.errorstring_of_token x
+        ^ ". " ^ fail_string)
