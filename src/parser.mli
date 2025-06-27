12ac23615ac3319a44ceaa32f9c58e6f4af91242
uncodem <emurex185@gmail.com>
2025-06-28 07:48:04 +0800

    Added interface files for lexer and parser

---
+++ src/parser.mli
@@ -0,0 +1,31 @@
+
+type parserstate
+
+type parser_error = UnexpectedToken of Lexer.token * string | UnexpectedEOF
+
+type binop = Add | Sub | Mul | Div | Assign | Eql | Neql | More | Less | EqMore | EqLess | Not
+
+type ast_node =
+    | Number of int
+    | Unary of binop * ast_node
+    | Binary of binop * ast_node * ast_node
+    | Statement of string * ast_node list
+    | Block of ast_node list
+    | Call of string * ast_node list
+    | Var of string
+
+val parser_init : Lexer.token list -> parserstate
+(** Generates parserstate from token list *)
+
+val parse_all : parserstate -> (ast_node list, parser_error) result
+(** Generates AST from provided parserstate *)
+
+val parser_report : parser_error -> unit
+(** Prints error message from parser. *)
+
+val string_of_ast : ast_node -> string
+(** Stringify ast node *)
+
+val print_parserstate : parserstate -> unit
+(** Prints current parser token stream for debugging *)
+
