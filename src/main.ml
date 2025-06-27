3894f7b84f776e57eeb0dec8d296e0b1d1c620df
uncodem <emurex185@gmail.com>
2025-06-27 18:12:40 +0800

    Made main.ml take input from a file

--- src/main.ml
+++ src/main.ml
@@ -1,10 +1,20 @@
+let read_lines fname = 
+    let ic = open_in fname in
+    let rec aux acc =
+        match input_line ic with
+        | line -> aux (line :: acc)
+        | exception End_of_file -> 
+            close_in ic; 
+            String.concat "\n" (List.rev acc)
+    in 
+    aux []
+
 let () =
-    let res =
-        Lexer.lexer_init "random_functoin (); input (12, 23, [pint (12, 23); output (45, [other ();]); ]);"
+    let res = 
+        Lexer.lexer_init (read_lines "test.txt") 
         |> Parser.parser_init |> Parser.parse_all
     in
     match res with
     | Ok a -> List.iter (fun node -> print_endline (Parser.string_of_ast node)) a
     | Error e -> Parser.parser_report e
-
 
