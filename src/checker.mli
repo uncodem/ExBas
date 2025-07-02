1ad30ea1a7c2d76061b3751032df2ef1951f0a2f
uncodem <emurex185@gmail.com>
2025-07-03 02:09:38 +0800

    Added checker.mli file

---
+++ src/checker.mli
@@ -0,0 +1,20 @@
+type node_type =
+    | T_int
+    | T_float
+    | T_string
+    | T_bool
+    | T_array of node_type
+    | T_none
+    | T_any
+
+type typed_node = {
+    kind: node_type;
+    node: Parser.ast_node;
+}
+
+type checker_error =
+    | MismatchedTypes of node_type * node_type
+    | ExpectedType of node_type * node_type
+
+val annotate_node : Parser.ast_node -> (typed_node, checker_error) result
+(** Annotates all the nodes of an ast recursively *)
