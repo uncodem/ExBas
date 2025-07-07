fe16f66e5460094ae63519b374e3e05e9aa16cfb
uncodem <emurex185@gmail.com>
2025-07-07 11:16:11 +0800

    Added doc comments to ast_nodes

--- src/parser.mli
+++ src/parser.mli
@@ -18,29 +18,29 @@ type binop =
     | Or
 
 type ast_node =
-    | Program of ast_node list
-    | Number of int
-    | String of string
-    | Bool of bool 
-    | Float of float
-    | Unary of binop * ast_node
-    | Binary of binop * ast_node * ast_node
-    | Statement of string * ast_node list * Lexer.token_pos
-    | Block of ast_node list
-    | Call of string * ast_node list
-    | Index of ast_node * ast_node
-    | Var of string
-    | If of ast_node * ast_node * ast_node option * Lexer.token_pos option
-    | Let of string * ast_node * Lexer.token_pos
-    | Assign of ast_node * ast_node * Lexer.token_pos option
-    | Label of string * Lexer.token_pos
-    | FuncDef of string * (string * string) list * string * ast_node * Lexer.token_pos
-    | Return of ast_node option * Lexer.token_pos
-    | While of ast_node * ast_node * Lexer.token_pos
-    | For of ast_node * ast_node * ast_node * ast_node * Lexer.token_pos
-    | Goto of string * Lexer.token_pos
-    | Yield of ast_node option * Lexer.token_pos
-    | Dim of string * ast_node list * string * Lexer.token_pos
+    | Program of ast_node list (** Root node of the AST. List of statements [stmts]*)
+    | Number of int (** Leaf node of type int. *)
+    | String of string (** Leaf node of type string. *)
+    | Bool of bool  (** Leaf node of type bool. *)
+    | Float of float (** Leaf node of type float. *)
+    | Unary of binop * ast_node (** Node for unary expressions (!,-). [op, right]*)
+    | Binary of binop * ast_node * ast_node (** Node for binary expressions (+,-,*,/, ...) [op, left, right]*)
+    | Statement of string * ast_node list * Lexer.token_pos (** Node for statement-level function calls. [funcname, params]*)
+    | Block of ast_node list (** Node for storing statements. Simply a list of statements. [stmts]*)
+    | Call of string * ast_node list (** Node for expression-level function calls. [funcname, params] *)
+    | Index of ast_node * ast_node (** Indexing node. [left, right] *)
+    | Var of string (** Leaf node for variables. [varname] *)
+    | If of ast_node * ast_node * ast_node option * Lexer.token_pos option (** Node for ifs. Could either be statement or expression. [cond, if, else]*)
+    | Let of string * ast_node * Lexer.token_pos (** Node for variable declaration. [varname, varval] *)
+    | Assign of ast_node * ast_node * Lexer.token_pos option (** Node for assignment on Var or Index. Could either be statement or expression. [left, right] *)
+    | Label of string * Lexer.token_pos (** Node for denoting labels. [labelname] *)
+    | FuncDef of string * (string * string) list * string * ast_node * Lexer.token_pos (** Node for function definition. [funcname, paramlist, returntype] *)
+    | Return of ast_node option * Lexer.token_pos (** Node for denoting returns. [retexpr_opt] *)
+    | While of ast_node * ast_node * Lexer.token_pos (** Node for denoting while loops. [cond, body] *)
+    | For of ast_node * ast_node * ast_node * ast_node * Lexer.token_pos (** Node for denoting for loops. [base, dest, step, body] *)
+    | Goto of string * Lexer.token_pos (** Node for denoting gotos. [labelname] *)
+    | Yield of ast_node option * Lexer.token_pos (** Node for denoting yields. [yieldexpr_opt] *)
+    | Dim of string * ast_node list * string * Lexer.token_pos (** Node for denoting dims. [varname, sizes, typing] *)
 
 val parser_init : Lexer.token list -> parserstate
 (** Generates parserstate from token list *)
